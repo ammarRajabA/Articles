3b9470f213e118d802f69d7916e4586194dca39c
Ammar Rajab <ammar.rajab1@gmail.com>
2019-08-29 11:18:54 +0300

    init

--- src/App.js
+++ src/App.js
@@ -23,7 +23,6 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state={showLogin:false}
-    localStorage.clear();
     this.user=JSON.parse(localStorage.getItem('user'))
     if (this.user) history.push('/articles')
   }
